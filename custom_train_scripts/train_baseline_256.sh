af6b1b5f683e7f29bcbd87de56184632c6b9b855
stoplime <slanimero@gmail.com>
2019-04-06 12:34:40 -0400

    Revert "train with full sequence cityscapes"
    This reverts commit cac50cea98e179359943c33e82b1647a94645c13.

--- custom_train_scripts/train_baseline_256.sh
+++ custom_train_scripts/train_baseline_256.sh
@@ -1,5 +1,4 @@
 python train.py --name baseline_256 \
---label_nc 35 --loadSize 256 --fg \
+--label_nc 35 --loadSize 256 --use_instance --fg \
 --n_downsample_G 2 --num_D 1 \
---max_frames_per_gpu 3 --n_frames_total 6 \
---dataroot datasets/full_Cityscapes/
+--max_frames_per_gpu 3 --n_frames_total 6
